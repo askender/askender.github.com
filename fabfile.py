71a2cf51462a705e70a78174888c55e7cc6f8ef0
askender <askender43@gmail.com>
2015-02-01 16:59:12 +0800

    change cname

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,5 @@
 from fabric.api import local
+from datetime import datetime
 
 
 def commit():
@@ -16,3 +17,36 @@ def push():
 def deploy():
     commit()
     push()
+
+
+TEMPLATE = """
+{title}
+{hashes}
+
+:date: {year}-{month}-{day} {hour}:{minute:02d}
+:tags:
+:category:
+:slug: {slug}
+:summary:
+:status: draft
+
+
+"""
+
+
+def make_entry(title):
+    today = datetime.today()
+    slug = title.lower().strip().replace(' ', '_')
+    f_create = "content/{}{:0>2}{:0>2}_{}.md".format(
+        str(today.year)[2:], today.month, today.day, slug)
+    t = TEMPLATE.strip().format(title=title,
+                                hashes='#' * len(title),
+                                year=today.year,
+                                month=today.month,
+                                day=today.day,
+                                hour=today.hour,
+                                minute=today.minute,
+                                slug=slug)
+    with open(f_create, 'w') as w:
+        w.write(t)
+    print("File created -> " + f_create)
